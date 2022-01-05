ce15c2f8b2e899478718aa3b57aab91bbd5443db
amir7896 <amirshahzad07896@gmail.com>
2022-01-06 01:33:12 +0500

    deploy app

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ db.once('open', () => {
 // Static files
 app.use(express.static(path.join(__dirname, '../frontend/dist/frontend')));
 // Angular app
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(
         path.join(__dirname, '../frontend/dist/frontend/index.html')
     );
