6a40b14907d88c4f87f006c407d7954fd96af738
amir7896 <amirshahzad07896@gmail.com>
2022-05-13 11:29:46 +0500

    Some Changing in code

--- server.js
+++ server.js
@@ -1,82 +1,77 @@
-if(process.env.NODE_ENV !== "production"){
-    require('dotenv').config()
-  }
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const dbcon = process.env.DB;
 
-
-const express = require('express');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const User = require('./models/user');
-const jwt  = require('jsonwebtoken')
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const User = require("./models/user");
+const jwt = require("jsonwebtoken");
+const path = require("path");
+const cors = require("cors");
 // ==================
-// Require For Login 
+// Require For Login
 // ==================
-const passport = require('passport');
-const LocalStrategy =  require('passport-local');
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 
 // ========================================
 // Routes For Hnadling The Functionaliteis
 // ========================================
-const campgroundRoutes = require('./routes/campgroounds');
-const reviewRoutes = require('./routes/review');
-const userRoutes  = require('./routes/user');
-const resetRoutes  = require('./routes/resetPassword');
+const campgroundRoutes = require("./routes/campgroounds");
+const reviewRoutes = require("./routes/review");
+const userRoutes = require("./routes/user");
+const resetRoutes = require("./routes/resetPassword");
 
 const app = express();
-
 
 // =======================
 // Session Configuration
 // =======================
 const sessionConfig = {
-    secret: "Thisinnotagoodsecret",
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        expired: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}
+  secret: "Thisinnotagoodsecret",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expired: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
 // =====================
 // Data Base Connection
 // ====================
 mongoose.connect(dbcon, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-   
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', () => {
-    console.log('Data Base Connected Successfully!');
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  console.log("Data Base Connected Successfully!");
 });
 
 // Static files
-app.use(express.static(path.join(__dirname, '../frontend/dist/frontend')));
+app.use(express.static(path.join(__dirname, "../frontend/dist/frontend")));
 
 app.use(session(sessionConfig));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 
 // ======================
 // For Login Use Of App
 // ======================
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new  LocalStrategy(User.authenticate()));
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
-
-
+app.use(cors("*"));
 
 // ================================================
 // Fake User For Checking our passport Work Or not
@@ -93,20 +88,16 @@ passport.deserializeUser(User.deserializeUser());
 // =====================
 // Use Routes Directory
 // =====================
-app.use('/', userRoutes);
-app.use('/campgrounds', campgroundRoutes);
-app.use('/campgrounds/:id/reviews', reviewRoutes);
-app.use('/resetPass', resetRoutes);
-
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
+app.use("/resetPass", resetRoutes);
 
 // Angular app
-app.get('*', (req, res) => {
-    res.sendFile(
-        path.join(__dirname, '../frontend/dist/frontend/index.html')
-    );
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/frontend/index.html"));
 });
 
-
 app.listen(port, () => {
-    console.log('Server Start On Port 8080');
-})
+  console.log("Server Start On Port 3000");
+});
